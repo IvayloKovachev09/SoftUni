3b65650c5eeffd46de4eaed1f87c317987ade846
IvayloKovachev <ivailokovachev@abv.bg>
2016-11-21 22:51:16 +0200

    Add Projects Groupe

---
+++ Programming Fundamentals/AllInOne_blog/AllInOne_blog/Models/Post.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AllInOne_blog.Models
+{
+    public class Post
+    {
+        public Post()
+        {
+            this.Date = DateTime.Now;
+        }
+        [Key]
+        public int ID { get; set; }
+
+
+        [Required]
+        public string Body { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        
+        public ApplicationUser Author { get; set; }
+
+
+    }
+}
---
+++ Programming Fundamentals/AllInOne_blog/AllInOne_blog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AllInOne_blog.Startup))]
+namespace AllInOne_blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
