7257065fe9e0fe59c7c8382b5a85abf6f839443d
IvayloKovachev <ivailokovachev@abv.bg>
2016-11-30 22:09:58 +0200

    delete Kinoman

--- Programming Fundamentals/AllInOne_blog/AllInOne_blog/Models/ApplicationDbContext.cs
+++ Programming Fundamentals/AllInOne_blog/AllInOne_blog/Models/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace AllInOne_blog.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
--- Programming Fundamentals/AllInOne_blog/AllInOne_blog/Models/Post.cs
+++ Programming Fundamentals/AllInOne_blog/AllInOne_blog/Models/Post.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -15,15 +16,22 @@ namespace AllInOne_blog.Models
         [Key]
         public int ID { get; set; }
 
+        [Required]
+        [StringLength(250)]
+        public string Title { get; set; }
 
         [Required]
+        [DataType(DataType.MultilineText)]
         public string Body { get; set; }
 
         [Required]
         public DateTime Date { get; set; }
 
-        
+        public string Author_Id { get; set; }
+        [ForeignKey("Author_Id")]
         public ApplicationUser Author { get; set; }
+       
+        public ICollection<Comment> Comments { get; set; }
 
 
     }
